2411ecad4d26a6ff9c89be2b29c68d9ca66f8e58
Isaias <isaias.alfaro@ucr.ac.cr>
2026-06-15 22:41:05 -0600

    configuracion incial de parametros

--- src/config.py
+++ src/config.py
@@ -1,5 +1,19 @@
 import torch
 
+import torch
+
+# Modelo y dataset
 MODEL_NAME = "dccuchile/bert-base-spanish-wwm-uncased"
 DATASET_NAME = "lpsc-fiuba/melisa"
 MAX_LEN = 128
+
+# Hiperparámetros de entrenamiento
+EPOCHS = 3
+BATCH_SIZE = 32
+LEARNING_RATE = 2e-5
+TRAIN_SPLIT = 0.9
+SEED = 42
+
+# Sistema
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+MODEL_SAVE_PATH = "model/"
